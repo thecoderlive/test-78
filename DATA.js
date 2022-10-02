9a8923e074740444e007adb07e8f12c0c432ad0b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-02 12:31:17 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "9c97cace-c99d-4b2b-b6c8-4e404a7c4529",
+    "home_feed": [
+        {
+            "id": "c3a40b69-d543-4eea-bdb3-b31e120522b5",
+            "artist_name": "coach",
+            "home_carousel": [
+                {
+                    "id": "80bfb9cd-5b10-40d9-9275-f8e8bb47398a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "4e75c449-5820-461a-8a24-65a95059f786",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "2683b1d5-cdca-45db-b9fc-0b94ee2d27a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "33cd2c7b-c6fe-4f04-9fd1-92a85577fd7e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                },
+                {
+                    "id": "06173d47-ab61-48e4-a98b-a7def5d71f7a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                }
+            ],
+            "art_caption": "field",
+            "art_description": "Under oil discover church visit parent billion body. Fight describe agency such quality that it lose."
+        },
+        {
+            "id": "3a68886f-82b7-48bb-a1ba-1f6e026ad9b5",
+            "artist_name": "career",
+            "home_carousel": [
+                {
+                    "id": "daaeadfb-21b8-40c8-9548-aa322a7ba5b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "ce92cde7-4f68-4d86-baae-c6c4f50c94ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/61.png"
+                },
+                {
+                    "id": "340a9409-9852-4161-9864-bbd331daae25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "59feacb7-f443-4e02-8bf1-7a851ca8697a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "5a362463-2afb-4083-88c6-9357fcb56f1d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                }
+            ],
+            "art_caption": "beyond",
+            "art_description": "Free realize stop ever. Manage note line born well fast argue space."
+        },
+        {
+            "id": "8dd6f787-2e46-4b08-8ca5-5de50e87355e",
+            "artist_name": "second",
+            "home_carousel": [
+                {
+                    "id": "204fbaee-7da1-4e33-9e79-9c55b2a9f2ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                },
+                {
+                    "id": "21159274-2780-44cc-bcad-95d262715794",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "ea977b52-0c9f-4d9f-baf8-21e35f2613c0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "4d8ce64e-95f1-440e-bff9-27d2910aae6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "0eaafda4-6473-4d49-a98d-c6352bbc233b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png"
+                }
+            ],
+            "art_caption": "sure",
+            "art_description": "Third us attorney follow trade research soldier. Describe nothing make of."
+        },
+        {
+            "id": "d48dc114-ae79-4aea-9142-0b9e674df123",
+            "artist_name": "send",
+            "home_carousel": [
+                {
+                    "id": "a4a214b8-1ba7-4b89-ac87-fc8a17cda7ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "e533a72d-bf71-4f27-b6ee-a4d93f60391d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "f861357b-73d3-4748-b26a-7a722e35ebab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "219e38b2-bbf3-4248-94f4-a75708f9b630",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "bb9ee536-6ee2-43ae-849b-edc57b17c7ad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                }
+            ],
+            "art_caption": "billion",
+            "art_description": "Test what group nor deal tonight."
+        },
+        {
+            "id": "e4a16dec-7d41-42f4-90a6-a629851367eb",
+            "artist_name": "away",
+            "home_carousel": [
+                {
+                    "id": "47c96591-aaa6-46d2-9d40-0bb20c6720e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+                },
+                {
+                    "id": "880105da-bb50-4c07-9a90-a3cfed2d0e76",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "a76bcff0-9135-4539-a6e0-19c12721137b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "f5378839-8099-4a84-b190-8bebdfcd31df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "440d3f81-d734-46d4-a025-2a98cef50def",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                }
+            ],
+            "art_caption": "truth",
+            "art_description": "Executive carry contain audience charge today. Relationship write once. Special look hand enter development."
+        }
+    ]
+}
